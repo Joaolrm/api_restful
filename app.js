48ffdc7422e3064a9b0aa5e78b0e08804a840c5f
unknown <jlluismr86@gmail.com>
2024-07-15 00:48:37 -0300

    2024-07-15-00-48 Correções de nomenclaturas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORTA = 3000;
 
 
 const loginController = require('./controller/login_controller');
-const servicoRealizado_controller = require('./rotas/servicoRealizado_rotas');
+const servicoRealizado_rotas = require('./rotas/servicoRealizado_rotas');
 const middlewareAcesso = require('./middleware/acesso_middleware');
 
 app.use(express.json())
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.post("/api/login", loginController.realizarLogin);
 
-app.use("/api/servicoRealizado", middlewareAcesso.verificarAcesso, servicoRealizado_controller);
+app.use("/api/servicoRealizado", middlewareAcesso.verificarAcesso, servicoRealizado_rotas);
 
 app.listen(PORTA, () => {
     console.log("Iniciando o servidor na porta " + PORTA);
