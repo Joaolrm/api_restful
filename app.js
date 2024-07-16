dc84a2441fa65b5c4c86d32c02d1ece113d4c248
Joao Luis Rosa de Moura <Joao.Moura@zaffari.com.br>
2024-07-16 16:49:39 -0300

    2024-07-16-16-49 Coreção do app

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ const PORTA = 3000;
 
 const loginController = require("./controller/login_controller");
 const servicoRealizado_rotas = require("./rotas/servicoRealizado_rotas");
+const barbeiro_rotas = require("./rotas/barbeiro_router");
 const middlewareAcesso = require("./middleware/acesso_middleware");
 
 app.use(express.json());
 
-// Middleware para logging
 app.use((req, res, next) => {
   console.log(req.method + " " + req.originalUrl);
   next();
 });
 
-// Rota para login
 app.post("/api/login", loginController.realizarLogin);
 
-// Rotas protegidas pelo middleware de acesso
 app.use(
   "/api/servicoRealizado",
   middlewareAcesso.verificarAcesso,
   servicoRealizado_rotas
+);
+
+app.use(
+  "/api/barbeiro",
+  middlewareAcesso.verificarAcesso,
+  barbeiro_rotas
 );
 
 app.listen(PORTA, () => {
