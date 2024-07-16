5934eef58c4a714f5b5bd71daf5271b7d96878b5
Joao Luis Rosa de Moura <Joao.Moura@zaffari.com.br>
2024-07-16 16:11:35 -0300

    2024-07-16-16-11 Consumo do router do barbeiro no app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORTA = 3000;
 
 const loginController = require("./controller/login_controller");
 const servicoRealizado_rotas = require("./rotas/servicoRealizado_rotas");
+const barbeiro_rotas = require("./rotas/barbeiro_router");
 const middlewareAcesso = require("./middleware/acesso_middleware");
 
 app.use(express.json());
@@ -19,6 +20,12 @@ app.use(
   "/api/servicoRealizado",
   middlewareAcesso.verificarAcesso,
   servicoRealizado_rotas
+);
+
+app.use(
+  "/api/barbeiro",
+  middlewareAcesso.verificarAcesso,
+  barbeiro_rotas
 );
 
 app.listen(PORTA, () => {
